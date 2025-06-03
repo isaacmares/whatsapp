aead85a134c0afaf7a86604ab12e7d96b4ff799d
IsaacMares <56515848+isaacmares@users.noreply.github.com>
2025-06-02 23:01:29 -0600

    Add files via upload

---
+++ crearServer.sh
@@ -0,0 +1,37 @@
+apt install npm
+mkdir whatsapp
+cd whatsapp
+
+apt install -y gconf-service libgbm-dev libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+
+apt update && apt install -y \
+    libasound2t64 \
+    libatk-bridge2.0-0t64 \
+    libatk1.0-0t64 \
+    libcups2t64 \
+    libdbus-1-3 \
+    libgdk-pixbuf2.0-0 \
+    libnspr4 \
+    libnss3 \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    xdg-utils \
+    libu2f-udev \
+    libvulkan1 \
+    libglu1-mesa \
+    fonts-liberation \
+    ca-certificates \
+    wget \
+    libappindicator3-1
+
+npm install whatsapp-web.js
+
+npm install qrcode
+npm install express
+npm install multer
+npm install mime-types
+
+wget wget https://github.com/isaacmares/whatsapp/archive/refs/heads/main.zip
+unzip main.zip
